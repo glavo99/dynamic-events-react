e325f72315383e48e5b02b183a47b1d52097a144
glavo99 <elgendy.yousef.1999@gmail.com>
2023-03-11 00:30:28 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { useState } from "react";
-import Heading from "./Heading";
+import Counter from "./components/Counter";
+import MealsList from "./components/MealsList";
+import MealsProvider from "./provider/MealsProvider";
 import "./App.css";
 function App() {
-  const [word, setWord] = React.useState("Eat");
-  // can't use the state's setting variable from your state directly.
-  //  Instead of updating it directly, I can update it based on a click event.
-  function handleClick() {
-    setWord("drink");
-  }
   return (
     <div className="App">
-      <Heading message={word + " at little lemon"} />
-      <button onClick={handleClick}>Click Here</button>
+      <MealsProvider>
+        <MealsList />
+        <Counter />
+      </MealsProvider>
     </div>
   );
 }
---
+++ src/provider/MealsProvider.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MealsContext = React.createContext();
+
+const todayMeals = ["Baked Beans", "Baked Sweat Potatoes", "Baked Potatoes"];
+
+const MealsProvider = ({ children }) => {
+  const [meals, setMealsList] = React.useState(todayMeals);
+  return (
+    <MealsContext.Provider value={{ meals }}>{children}</MealsContext.Provider>
+  );
+};
+
+export const useMealsListContext = () => React.useContext(MealsContext);
+
+export default MealsProvider;
